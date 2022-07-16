f793de22106e72df0fbed07e63423cf483cb3ad7
anupam-tiwari <appleanupam@gmail.com>
2022-07-16 22:45:30 +0530

    added image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import landing from '../assets/landing.jpeg'
 
 const Home = () => {
   return (
     <div>
         <Navbar></Navbar>
+        <div className='flex justify-center'>
+          <img src={landing}></img>
+        </div>
     </div>
   )
 }
